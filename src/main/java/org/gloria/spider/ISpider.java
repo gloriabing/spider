44b6ae209f17f38f6ffbf95db8aaec6a391e8a46
Gloria <zhangbing1990@126.com>
2017-03-13 18:25:20 +0800

    爬虫抓取流程接口

---
+++ src/main/java/org/gloria/spider/ISpider.java
@@ -0,0 +1,36 @@
+package org.gloria.spider;
+
+import org.gloria.config.Response;
+import org.gloria.config.Seed;
+
+import java.util.List;
+
+/**
+ * Create on 2017/3/13 15:44.
+ *
+ * @author : gloria.
+ */
+public interface ISpider {
+
+    /**
+     * 初始化爬虫配置
+     *
+     * @return
+     */
+    Seed init();
+
+    /**
+     * 解析内容
+     *
+     * @param response
+     */
+    void parse(Response response);
+
+    /**
+     * 发现更多内容
+     *
+     * @return
+     */
+    List<String> discoverMore(Response response);
+
+}
