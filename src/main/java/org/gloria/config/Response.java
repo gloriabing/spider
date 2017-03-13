c41a59dd949e9d83144c2f057c1254322eb27703
Gloria <zhangbing1990@126.com>
2017-03-13 18:25:49 +0800

    响应内容处理

---
+++ src/main/java/org/gloria/config/Response.java
@@ -0,0 +1,52 @@
+package org.gloria.config;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+/**
+ * Create on 2017/3/9 16:21.
+ *
+ * @author : gloria.
+ */
+public class Response {
+
+    private static String body;
+    private Template template;
+    
+    public Response body(String body) {
+        this.body = body;
+        return this;
+    }
+
+    public String body() {
+        return this.body();
+    }
+
+    public Response template(Template template) {
+        this.template = template;
+        return this;
+    }
+    public Document document() {
+        return Jsoup.parse(body);
+    }
+
+    /**
+     * dom解析
+     * @param selector
+     *          css选择器表达式
+     * @return
+     *          符合条件的元素集合
+     */
+    private Elements elements(String selector) {
+        return document().select(selector);
+    }
+
+    public Elements domParse() {
+        if (template.type() == Type.HTML) {
+            return elements(template.expression());
+        }
+        return new Elements();
+    }
+        
+}
