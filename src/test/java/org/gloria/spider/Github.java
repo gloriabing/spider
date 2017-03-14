4f93faa282c8bc1df836988a67aab005220bde12
Gloria <zhangbing1990@126.com>
2017-03-14 14:26:08 +0800

    github多页面抓取测试

--- src/test/java/org/gloria/spider/Github.java
+++ src/test/java/org/gloria/spider/Github.java
@@ -5,10 +5,12 @@ import org.gloria.config.Seed;
 import org.gloria.config.Template;
 import org.gloria.config.Type;
 import org.gloria.scheduler.Scheduler;
+import org.gloria.util.Regex;
 import org.jsoup.nodes.Element;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -39,7 +41,23 @@ public class Github implements ISpider {
 
     @Override
     public List<String> discoverMore(Response response) {
-        return null;
+        Template template = Template.me().type(Type.HTML).expression("a.next_page");
+        response.template(template);
+        Element element = response.domParse().first();
+        String url = element.attr("href");
+        url = "https://github.com" + url;
+        String finalUrl = url;
+        if (Regex.match(finalUrl, "^https://github\\.com/search\\?p=([\\d]+)\\&").equals("2")) {
+            Scheduler.shutdown();
+        }
+        return new ArrayList<String>() {{
+            add(finalUrl);
+        }};
+    }
+
+    @Override
+    public boolean shutdown() {
+        return false;
     }
 
     public static void main(String[] args) {
